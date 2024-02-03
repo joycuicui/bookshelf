f096cce12d7f066dfd51176693c2bc1189052127
joycuicui <cuiqijoy@outlook.com>
2024-02-03 12:28:41 -0800

    feat: add search icon; refactor header Links

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { HiMagnifyingGlass } from "react-icons/hi2";
 
 const Header = () => {
   return (
@@ -8,26 +9,35 @@ const Header = () => {
           <span className="text-emerald-600">Book</span>
           <span className="text-emerald-800">Haven</span>
         </h1>
-        <form className="bg-emerald-100 p-3 rounded-full">
+        <form className="bg-emerald-100 p-1 rounded-full flex items-center gap-2">
           <input
             type="text"
             placeholder="Search..."
-            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            className="outline-none w-28 rounded-full bg-emerald-100 px-4 py-2  transition-all duration-300 focus:ring focus:ring-emerald-300 focus:ring-opacity-50 sm:w-72 sm:focus:w-96"
           />
+          <HiMagnifyingGlass className="text-emerald-600 mr-2 text-lg cursor-pointer" />
         </form>
         <ul className="flex gap-4 text-sm sm:text-lg text-emerald-900 font-medium">
-          <Link to="/">
-            <li className="hidden sm:inline hover:underline">Home</li>
-          </Link>
-          <Link to="/about">
-            <li className="hidden sm:inline hover:underline">About</li>
-          </Link>
-          <Link to="/login">
-            <li className=" hover:underline">Log In</li>
-          </Link>
-          <Link to="/signup">
-            <li className=" hover:underline">Sign Up</li>
-          </Link>
+          <li>
+            <Link to="/" className="hidden sm:inline hover:underline">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="hidden sm:inline hover:underline">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/login" className=" hover:underline">
+              Log In
+            </Link>
+          </li>
+          <li>
+            <Link to="/signup" className=" hover:underline">
+              Sign Up
+            </Link>
+          </li>
         </ul>
       </div>
     </header>
