a61507cf73f8c1745db053147c4004825f9ea798
joycuicui <cuiqijoy@outlook.com>
2024-02-02 20:06:14 -0800

    feat: add routes for home, about, signup, login

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Header from "./components/Header";
+
 const App = () => {
-  return <div className="text-3xl font-bold underline bg-yellow-100">App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
