dacb9c4fd7eabe612af70d62cc1dbee687cfef75
joycuicui <cuiqijoy@outlook.com>
2024-02-03 10:52:54 -0800

    feat: add error handling middleware

--- back-end/server.js
+++ back-end/server.js
@@ -20,6 +20,18 @@ const authRouter = require("./routes/auth-router");
 // app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 
+// error-handling middleware - catch errors and send error response to the client
+// trigger this middleware by calling next() with an error object in route handlers
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
 });
