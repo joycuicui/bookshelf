5179ee8bdef4bec68c79f404e5c14ea5e509c91f
joycuicui <cuiqijoy@outlook.com>
2024-02-02 21:15:18 -0800

    add json middleware & auth router

--- back-end/server.js
+++ back-end/server.js
@@ -10,11 +10,15 @@ const port = process.env.PORT || 8000;
 
 // middleware
 app.use(morgan("dev"));
+// parsing json data
+app.use(express.json());
 
 // routes
-const userRouter = require("./routes/user-router");
+// const userRouter = require("./routes/user-router");
+const authRouter = require("./routes/auth-router");
 
-app.use("/api/users", userRouter);
+// app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
