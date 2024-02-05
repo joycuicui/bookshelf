2167059ca5d8863b97c08b3ffeb0ca1b7ecc8cf2
joycuicui <cuiqijoy@outlook.com>
2024-02-04 17:36:37 -0800

    feat: add user page layout

--- front-end/src/pages/UserPage.jsx
+++ front-end/src/pages/UserPage.jsx
@@ -1,5 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+
 const UserPage = () => {
-  return <div>UserPage</div>;
+  return (
+    <>
+      <Sidebar />
+      <main className="ml-64 bg-gray-50">
+        <Outlet />
+      </main>
+    </>
+  );
 };
 
 export default UserPage;
