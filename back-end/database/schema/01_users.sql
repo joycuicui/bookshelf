1fd42059521eb21189344f9e0474b520865f5ebf
joycuicui <cuiqijoy@outlook.com>
2024-02-05 11:55:24 -0800

    feat: add default avatar

--- back-end/database/schema/01_users.sql
+++ back-end/database/schema/01_users.sql
@@ -6,5 +6,5 @@ CREATE TABLE users (
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
-  avatar VARCHAR(255)
+  avatar VARCHAR(255) DEFAULT '/default-user.jpg'
 );
