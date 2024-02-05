3d989eadcb59075b489719cb8ee91eb0fa4d0285
joycuicui <cuiqijoy@outlook.com>
2024-02-05 11:55:49 -0800

    feat: create one user with avatar

--- back-end/database/seeds/01_users.sql
+++ back-end/database/seeds/01_users.sql
@@ -1,1 +1,3 @@
 -- Users table seeds here (Example)
+INSERT INTO users (name, email, password, avatar)
+VALUES ('alice', 'alice@email.com', '$2b$10$w9qH4SCUiGSEEhHXQCdq/.7rG2TQ5khZWZiqu6mnGSdH35eultiba', 'alice-flower-dog.jpg');
