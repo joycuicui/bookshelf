44ce799fb0c3deed28accd61097ee85b20e9d5e1
joycuicui <cuiqijoy@outlook.com>
2024-02-06 13:36:06 -0800

    chore: add testing seed data

---
+++ back-end/database/seeds/02_books.sql
@@ -0,0 +1,7 @@
+-- no cover image for now - will use default cover image
+INSERT INTO books (id, title, description, publisher, published_date, isbn, external_id) VALUES
+(1, 'The Great Gatsby', 'desc for great gatsby', 'Scribner', '1925-04-10', '9780743273565', '9780743273565'),
+(2, 'To Kill a Mockingbird', 'desc for mocking bird', 'J.B. Lippincott & Co.', '1960-07-11', '9780061120084', '9780061120084'),
+(3, '1984', 'desc for 1984', 'Secker & Warburg', '1949-06-08', '9780451524935', '9780451524935'),
+(4, 'The Catcher in the Rye', 'desc for The Catcher in the Rye', 'Little, Brown and Company', '1951-07-16', '9780316769488', '9780316769488'),
+(5, 'The Grapes of Wrath', 'desc for grapes', 'The Viking Press', '1939-04-14', '9780143039433', '9780143039433');
---
+++ back-end/database/seeds/03_authors.sql
@@ -0,0 +1,6 @@
+INSERT INTO authors (id, name) VALUES
+(1, 'F. Scott Fitzgerald'),
+(2, 'Harper Lee'),
+(3, 'George Orwell'),
+(4, 'J.D. Salinger'),
+(5, 'John Steinbeck');
