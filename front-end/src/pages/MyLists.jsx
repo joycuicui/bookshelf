59abb7cd6c0ec6f90759379f4abf21aa3e92d5df
joycuicui <cuiqijoy@outlook.com>
2024-02-06 10:00:46 -0800

    feat: use reading list global state in MyLists page

--- front-end/src/pages/MyLists.jsx
+++ front-end/src/pages/MyLists.jsx
@@ -1,4 +1,46 @@
+import { useSelector } from "react-redux";
+
 const MyLists = () => {
-  return <div>MyLists</div>;
+  const { wantToRead, currentlyReading, finishedReading } = useSelector(
+    (state) => state.readingList.lists
+  );
+
+  console.log(wantToRead);
+
+  return (
+    <div>
+      <div>
+        Want to Read
+        <ul>
+          {wantToRead?.map((book) => (
+            <li key={book.id}>
+              <span className="bg-red-200">{book.title}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        Currently Reading
+        <ul>
+          {currentlyReading?.map((book) => (
+            <li className="bg-red-200" key={book.id}>
+              {book.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        Finished
+        <ul>
+          {finishedReading?.map((book) => (
+            <li className="bg-red-200" key={book.id}>
+              {book.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
+
 export default MyLists;
