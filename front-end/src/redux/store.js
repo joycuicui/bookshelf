b21df353edf0851984973cb614fefb3f42272f6b
joycuicui <cuiqijoy@outlook.com>
2024-02-06 19:45:30 -0800

    feat: remove reading lists from redux

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -2,11 +2,9 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userReducer from "./user/userSlice.js";
-import readingListReducer from "./readingList/readingListSlice.js";
 
 const rootReducer = combineReducers({
   user: userReducer,
-  readingList: readingListReducer,
 });
 
 const persistConfig = {
