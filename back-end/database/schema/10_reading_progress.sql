458130335ccb93c16aca3a74a5f0f1d293230084
joycuicui <cuiqijoy@outlook.com>
2024-02-07 14:03:26 -0800

    fix: modify reading progress schema & seed

--- back-end/database/schema/10_reading_progress.sql
+++ back-end/database/schema/10_reading_progress.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS reading_progress CASCADE;
 CREATE TABLE reading_progress (
   id SERIAL PRIMARY KEY NOT NULL,
   -- name  VARCHAR(255) NOT NULL,
-  book_list_id INTEGER NOT NULL REFERENCES book_lists(id) ON DELETE CASCADE,
-  book_id INTEGER NOT NULL REFERENCES books(id) ON DELETE CASCADE,
+  user_id INTEGER NOT NULL REFERENCES book_lists(id) ON DELETE CASCADE,
+  book_author_id INTEGER NOT NULL REFERENCES book_authors(id) ON DELETE CASCADE,
   current_page INTEGER NOT NULL,
-  total_page INTEGER NOT NULL,
+  total_pages INTEGER NOT NULL,
   updated_at TIMESTAMP NOT NULL
   -- delta INTEGER NOT NULL
   );
--- back-end/database/seeds/10_reading_progress.sql
+++ back-end/database/seeds/10_reading_progress.sql
@@ -1,6 +1,6 @@
-INSERT INTO reading_progress (book_list_id, book_id, current_page, total_page, updated_at) VALUES
-(2 , 63 , 537, 652, '2024-02-07 10:30:00'),
-(2 , 12 , 10, 130, '2024-02-01 09:30:00')
+INSERT INTO reading_progress (user_id, book_author_id, current_page, total_pages, updated_at) VALUES
+(1 , 81 , 537, 652, '2024-02-07 10:30:00'),
+(1 , 17 , 10, 130, '2024-02-01 09:30:00')
 -- (3 , 3 , 100, 562, '2024-01-15 06:30:00'),
 -- (1 , 5 , 35, 393, '2023-01-15 08:30:00'),
 -- (1 , 2, 68, 128,'2024-01-30 08:30:00')
