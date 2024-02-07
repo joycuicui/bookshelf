9d0c2f4da9386c10bb2c80bacd2dd1262d9646eb
bahmed32 <bilqis.ali32@gmail.com>
2024-02-06 17:39:04 -0800

    added api call for search functionality

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -13,13 +13,16 @@ import MyLists from "./pages/MyLists";
 import MyReviews from "./pages/MyReviews";
 import MySettings from "./pages/MySettings";
 
+
 const App = () => {
+ 
+  
   return (
     <>
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
+          <Routes>
+            <Route path="/" element={<Home />} />
           <Route path="search" element={<Search />} />
           <Route path="about" element={<About />} />
           <Route path="login" element={<Login />} />
@@ -33,29 +36,32 @@ const App = () => {
             </Route>
           </Route>
         </Routes>
-      </BrowserRouter>
-      <Toaster
-        position="top-center"
-        gutter={12}
-        containerStyle={{ margin: "8px" }}
-        toastOptions={{
-          success: {
-            duration: 3000,
-          },
-          error: {
-            duration: 5000,
-          },
-          style: {
-            fontSize: "16px",
-            maxWidth: "500px",
-            padding: "16px 24px",
-            backgroundColor: "",
-            color: "",
-          },
-        }}
-      />
-    </>
-  );
+      
+     </BrowserRouter>
+     <Toaster
+       position="top-center"
+       gutter={12}
+       containerStyle={{ margin: "8px" }}
+       toastOptions={{
+         success: {
+           duration: 3000,
+         },
+         error: {
+           duration: 5000,
+         },
+         style: {
+           fontSize: "16px",
+           maxWidth: "500px",
+           padding: "16px 24px",
+           backgroundColor: "",
+           color: "",
+         },
+       }}
+     />
+   </>
+ );
 };
+
+
 
 export default App;
--- front-end/src/pages/Search.jsx
+++ front-end/src/pages/Search.jsx
@@ -1,4 +1,45 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
 const Search = () => {
-  return <div>Search</div>;
+  const location = useLocation();
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const searchTerm = new URLSearchParams(location.search).get("q");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (searchTerm) {
+          const response = await fetch(`https://openlibrary.org/search.json?q=${searchTerm}`);
+          const result = await response.json();
+          setData(result.docs);
+          setSearchResults(result.docs);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  }, [searchTerm]);
+
+  return (  
+    <div className="search-container p-4">
+      <ul>
+        {searchResults.map((item, i) => (
+          <li key={i} className="border-2 border-green-500 p-4 mb-4 ">
+            <strong>Title:</strong> {item.title}<br />
+            <strong>Author:</strong> {item.author_name && item.author_name.join(', ')}<br />
+            <strong>First Publish Year:</strong> {item.first_publish_year}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
+
 export default Search;
+
