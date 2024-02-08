a0522e24198349f1da75912f1e709276c4b30f01
joycuicui <cuiqijoy@outlook.com>
2024-02-08 14:20:09 -0800

    feat: render my reviews

--- front-end/src/pages/MyReviews.jsx
+++ front-end/src/pages/MyReviews.jsx
@@ -1,29 +1,31 @@
 import { useSearchParams } from "react-router-dom";
+import { HiArrowSmallDown, HiArrowSmallUp } from "react-icons/hi2";
+
 import SortBy from "../components/SortBy";
 import StarRating from "../components/StarRating";
+import { useReviews } from "../query/useReviews";
+import { useState } from "react";
 
 const MyReviews = () => {
+  const { isLoading, reviews } = useReviews();
   const [searchParams] = useSearchParams();
+
   // SORTING
-  // const sortBy = searchParams.get("sortBy") || "title-asc";
-  // const [field, direction] = sortBy.split("-");
-  // const modifier = direction === "asc" ? 1 : -1;
-  // const sortedBooks = filteredBooks?.slice().sort((a, b) => {
-  //   let aValue, bValue;
-  //   if (field === "progress") {
-  //     // Calculate progress as a percentage
-  //     const aProgress = a.current_page / a.total_pages;
-  //     const bProgress = b.current_page / b.total_pages;
-  //     aValue = aProgress;
-  //     bValue = bProgress;
-  //   } else {
-  //     aValue = a[field];
-  //     bValue = b[field];
-  //   }
-  //   if (aValue < bValue) return -1 * modifier;
-  //   if (aValue > bValue) return 1 * modifier;
-  //   return 0;
-  // });
+  const sortBy = searchParams.get("sortBy") || "title-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+
+  const sortedReviews = reviews?.slice().sort((a, b) => {
+    const aValue = a[field];
+    const bValue = b[field];
+    return (aValue < bValue ? -1 : aValue > bValue ? 1 : 0) * modifier;
+  });
+
+  const mappedReviews = sortedReviews?.map((book) => (
+    <MyReviewCard key={book.id} book={book} />
+  ));
+
+  if (isLoading) return <div className="spinner"></div>;
 
   return (
     <div>
@@ -43,17 +45,7 @@ const MyReviews = () => {
           ]}
         />
       </div>
-      {/* {sortedBooks?.map((book) => (
-      <MyBookItem key={book.id} book={book} />
-    ))} */}
-      <div className="flex">
-        <MyReviewCard
-          book={{ book_id: 100, title: "a book title", author: "lulus melons" }}
-        />
-        <MyReviewCard
-          book={{ book_id: 100, title: "a book title", author: "lulus melons" }}
-        />
-      </div>
+      <div className="flex flex-wrap gap-4">{mappedReviews}</div>
     </div>
   );
 };
@@ -61,12 +53,25 @@ const MyReviews = () => {
 export default MyReviews;
 
 const MyReviewCard = ({ book }) => {
-  const { book_id: bookId, title, author } = book;
-  const review = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero deleniti iste amet autem voluptas porro tempore harum minima animi. Labore quis perspiciatis iste. Inventore, modi? Temporibus ad esse minima odio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste libero exercitationem, ullam omnis fugit nihil odit impedit eos dolore deserunt sint sit voluptatem, obcaecati dolores? Dolor dolore deleniti architecto sequi. Lorem, ipsum dolor sit amet consectetur adipisicing elit.Porro, quibusdam culpa id ad nobis magnam consequatur inventore non!`;
+  const {
+    book_id: bookId,
+    title,
+    author,
+    cover_image,
+    first_published,
+    review,
+    rating,
+  } = book;
   // console.log(review.length);
 
+  const [showFullReview, setShowFullReview] = useState(false);
+
+  const toggleShowFullReview = () => {
+    setShowFullReview(!showFullReview);
+  };
+
   return (
-    <div className="mx-12 mt-8 p-3 flex flex-col gap-3 border border-gray-300 rounded-lg shadow w-1/2 h-96">
+    <div className="mx-12 mt-8 p-3 flex flex-col gap-3 border border-gray-300 rounded-lg shadow max-w-[46rem] h-[26rem] overflow-auto">
       <div className="flex justify-between gap-8">
         <img
           src="/default-cover-image.png"
@@ -75,18 +80,41 @@ const MyReviewCard = ({ book }) => {
           className="w-28 h-40 rounded-sm"
         />
         <div className="flex flex-col gap-2 flex-grow">
-          <p className="text-gray-700 font-semibold text-xl pt-2">{title}</p>
+          <p className="text-gray-700 font-semibold text-xl pt-2 truncate">
+            {title}
+          </p>
           <p className="italic">by {author}</p>
-          <div>average rating: 4.5</div>
-          <StarRating />
+          <div>First Published: {first_published}</div>
+          <StarRating ratedRating={rating} />
         </div>
         <div className="flex gap-3 items-start m-3">
           <button className="hover:underline text-emerald-600">EDIT</button>
           <button className="hover:underline text-red-600">DELETE</button>
         </div>
       </div>
-      <div className="border-t-[1px] p-3">
-        <p>{review.slice(0, 540)}</p>
+      <div className="border-t-[1px] p-3 mb-0 pb-0">
+        {showFullReview ? (
+          <p>{review}</p>
+        ) : (
+          <p>
+            {review.slice(0, 540)}{" "}
+            <span className="text-emerald-700 font-semibold">. . .</span>
+          </p>
+        )}
+        <button
+          onClick={toggleShowFullReview}
+          className="text-emerald-600 hover:underline mt-2"
+        >
+          {showFullReview ? (
+            <>
+              Show Less <HiArrowSmallUp className="inline text-lg" />
+            </>
+          ) : (
+            <>
+              Show More <HiArrowSmallDown className="inline text-lg" />
+            </>
+          )}
+        </button>
       </div>
     </div>
   );
