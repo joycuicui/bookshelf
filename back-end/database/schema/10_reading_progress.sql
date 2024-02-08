24c11ac762c5e9c447c1a661d130b50462229b1f
joycuicui <cuiqijoy@outlook.com>
2024-02-07 17:24:43 -0800

    fix: update reading_progress schema & seed

--- back-end/database/schema/10_reading_progress.sql
+++ back-end/database/schema/10_reading_progress.sql
@@ -4,7 +4,8 @@ DROP TABLE IF EXISTS reading_progress CASCADE;
 CREATE TABLE reading_progress (
   id SERIAL PRIMARY KEY NOT NULL,
   -- name  VARCHAR(255) NOT NULL,
-  user_id INTEGER NOT NULL REFERENCES book_lists(id) ON DELETE CASCADE,
+  book_list_id INTEGER NOT NULL REFERENCES book_lists(id) ON DELETE CASCADE,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   book_author_id INTEGER NOT NULL REFERENCES book_authors(id) ON DELETE CASCADE,
   current_page INTEGER NOT NULL,
   total_pages INTEGER NOT NULL,
