a52c1ebde2aff2d0ed89663f1e0a4bd7e579302e
joycuicui <cuiqijoy@outlook.com>
2024-02-09 11:01:44 -0800

    feat: add new review working

--- front-end/src/components/AddReview.jsx
+++ front-end/src/components/AddReview.jsx
@@ -1,16 +1,98 @@
+import { useState } from "react";
 import Slider from "react-slick";
-import StarRating from "./StarRating";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import StarRating from "./StarRating";
+import { useAddReview } from "../query/useAddReview";
+
 const AddReview = ({ books, onCloseModal }) => {
+  const { isAdding, addReview } = useAddReview();
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
-    <form
-      // onSubmit={handleSubmitEditReview}
-      className="overflow-hidden w-[800px] flex flex-col p-10 gap-6"
-    >
-      <BookCarousel books={books} />
-      <div className="flex justify-end gap-3 mt-5">
+    <div className="overflow-hidden w-[800px] flex flex-col p-10 gap-6">
+      <Slider {...settings}>
+        {books.map((book) => (
+          <BookCarousel
+            key={book.book_author_id}
+            book={book}
+            onCloseModal={onCloseModal}
+            addReview={addReview}
+          />
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default AddReview;
+
+const BookCarousel = ({ book, onCloseModal, addReview }) => {
+  const [newReview, setNewReview] = useState("");
+  const [newRating, setNewRating] = useState(0);
+
+  const {
+    book_author_id: bookAuthorId,
+    title,
+    author,
+    cover_image,
+    first_published,
+  } = book;
+
+  const handleNewRating = (newRating) => {
+    setNewRating(newRating);
+  };
+
+  const handleNewReview = (e) => {
+    setNewReview(e.target.value);
+  };
+
+  const handleSubmitNewReview = async (e) => {
+    e.preventDefault();
+    await addReview({ bookAuthorId, rating: newRating, review: newReview });
+    onCloseModal();
+  };
+
+  return (
+    <form onSubmit={handleSubmitNewReview}>
+      <div className="px-4">
+        <div className="flex justify-between gap-8">
+          <img
+            src={cover_image}
+            alt="Book Cover"
+            className="w-28 h-40 rounded-sm border"
+          />
+          <div className="flex flex-col gap-2 flex-grow">
+            <p className="text-gray-700 font-semibold text-xl pt-2 truncate">
+              {title}
+            </p>
+            <p className="italic">by {author}</p>
+            <div>First Published: {first_published}</div>
+            <StarRating
+              ratedRating={0}
+              isEditing={true}
+              onChange={handleNewRating}
+            />
+          </div>
+        </div>
+        <div>
+          <textarea
+            type="text"
+            onChange={handleNewReview}
+            placeholder="Write your review here..."
+            className="mt-3 p-2 border border-gray-300 shadow-sm rounded-sm w-full h-64"
+          />
+        </div>
+      </div>
+      <div className="flex justify-end gap-3 my-5">
         <button
           onClick={onCloseModal}
           type="reset"
@@ -26,53 +108,5 @@ const AddReview = ({ books, onCloseModal }) => {
         </button>
       </div>
     </form>
-  );
-};
-
-export default AddReview;
-
-const BookCarousel = ({ books }) => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
-  return (
-    <Slider {...settings}>
-      {books.map((book) => (
-        <div key={book.id} className="px-4">
-          <div className="flex justify-between gap-8">
-            <img
-              src={book.cover_image}
-              alt="Book Cover"
-              className="w-28 h-40 rounded-sm border"
-            />
-            <div className="flex flex-col gap-2 flex-grow">
-              <p className="text-gray-700 font-semibold text-xl pt-2 truncate">
-                {book.title}
-              </p>
-              <p className="italic">by {book.author}</p>
-              <div>First Published: {book.first_published}</div>
-              <StarRating
-                ratedRating={0}
-                isEditing={true}
-                // onChange={handleRatingChange}
-              />
-            </div>
-          </div>
-          <div>
-            <textarea
-              type="text"
-              // onChange={handleReviewChange}
-              // defaultValue={review}
-              className="mt-3 p-2 border border-gray-300 shadow-sm rounded-sm w-full h-64"
-            />
-          </div>
-        </div>
-      ))}
-    </Slider>
   );
 };
