19b7b4399225a294181b9dfb9dc0ced260f4d00e
joycuicui <cuiqijoy@outlook.com>
2024-02-08 22:00:21 -0800

    stye: fix cursor for SortBy component

--- front-end/src/components/SortBy.jsx
+++ front-end/src/components/SortBy.jsx
@@ -14,7 +14,7 @@ const SortBy = ({ options }) => {
       value={sortBy}
       onChange={handleChange}
       className="border py-2 px-4 rounded-md font-semibold transition duration-300 
-      text-gray-700"
+      text-gray-700 cursor-pointer"
     >
       {options.map((option) => (
         <option key={option.value} value={option.value}>
