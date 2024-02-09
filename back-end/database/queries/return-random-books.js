3c178b34982e7c51e8ec473347d101a686614c55
joycuicui <cuiqijoy@outlook.com>
2024-02-09 11:00:21 -0800

    chore: comment out console log from getRandomBooks

--- back-end/database/queries/return-random-books.js
+++ back-end/database/queries/return-random-books.js
@@ -7,9 +7,10 @@ const getRandomBooks = async () => {
       INNER JOIN book_authors ON books.id = book_authors.book_id
       INNER JOIN authors ON book_authors.author_id = authors.id
       ORDER BY RANDOM()
-      LIMIT 20;`);
+      LIMIT 20;`
+    );
     const tenBooks = res.rows;
-    console.log("return-ten-books.js!! :", tenBooks);
+    // console.log("return-ten-books.js!! :", tenBooks);
     return tenBooks;
   } catch (err) {
     console.log("failed in return-ten-books.js", err.message);
