85b9065156b01539a8af06393e66c5f182fc9ed0
joycuicui <cuiqijoy@outlook.com>
2024-02-08 16:25:38 -0800

    feat: update and delete review routes

--- back-end/routes/reviews-router.js
+++ back-end/routes/reviews-router.js
@@ -1,9 +1,16 @@
 const express = require("express");
+
 const verifyToken = require("../helpers/verifyUser");
-const { getAllReviews } = require("../controllers/review-controller");
+const {
+  getAllReviews,
+  editReview,
+  deleteReview,
+} = require("../controllers/review-controller");
 
 const router = express.Router();
 
 router.get("/:userId", verifyToken, getAllReviews);
+router.patch("/:reviewId", verifyToken, editReview);
+router.delete("/:reviewId", verifyToken, deleteReview);
 
 module.exports = router;
