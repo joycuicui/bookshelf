cbda927c6122455eab43bf13acfb7a93cad58e64
joycuicui <cuiqijoy@outlook.com>
2024-02-11 12:29:26 -0800

    fix: use links instead of local path for default image

--- back-end/database/seeds/01_users.sql
+++ back-end/database/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name, email, password, avatar)
-VALUES ('alice', 'alice@email.com', '$2b$10$w9qH4SCUiGSEEhHXQCdq/.7rG2TQ5khZWZiqu6mnGSdH35eultiba', 'alice-flower-dog.jpg');
+VALUES ('alice', 'alice@email.com', '$2b$10$w9qH4SCUiGSEEhHXQCdq/.7rG2TQ5khZWZiqu6mnGSdH35eultiba', 'https://github.com/joycuicui/bookshelf/blob/main/front-end/public/alice-flower-dog.jpg?raw=true');
