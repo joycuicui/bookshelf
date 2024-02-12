d4dbb92b60f078e20ee719e89c54fddb8b3074cd
c22quiambao <christelquiambao@gmail.com>
2024-02-12 14:02:41 -0800

    changes to home page and new book details page

--- back-end/routes/books-router.js
+++ back-end/routes/books-router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const getRandomBooks = require("../database/queries/return-random-books.js");
 const verifyToken = require("../helpers/verifyUser");
 const { addNewBook } = require("../controllers/book-controller");
+const getBookById = require("../database/queries/getBookById.js");
 
 const router = express.Router();
 
@@ -9,10 +10,28 @@ router.post("/:userId", verifyToken, addNewBook);
 
 router.get("/all", async (req, res) => {
   try {
-    const books = await getRandomBooks(); // Fetching books from the database
-    res.json(books); // Send the fetched books as a JSON response
+    const books = await getRandomBooks();
+    res.json(books);
   } catch (err) {
     console.error("Error fetching books:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log("book router - ID : ", id);
+
+    const book = await getBookById(id);
+    if (!book) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+    res.json(book); 
+    console.log("book router - book details : ", book)
+
+  } catch (err) {
+    console.error("Error fetching book details:", err);
     res.status(500).json({ error: "Internal server error" });
   }
 });
