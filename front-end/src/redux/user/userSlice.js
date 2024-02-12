040f41aab34905592f8e01848bd2ddce014d7361
joycuicui <cuiqijoy@outlook.com>
2024-02-11 18:23:20 -0800

    feat: add update and delete reducers

--- front-end/src/redux/user/userSlice.js
+++ front-end/src/redux/user/userSlice.js
@@ -28,6 +28,32 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = null;
     },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
@@ -35,8 +61,13 @@ export const {
   loginStart,
   loginSuccess,
   loginFailure,
-
   logoutSuccess,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
 } = userSlice.actions;
 
 export default userSlice.reducer;
